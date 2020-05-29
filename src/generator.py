48818385d7c93aabf0801d7bb7ebc8ab068aecdf
Khushal Sethi <iaskhushal@gmail.com>
2020-05-29 20:28:56 +0530

    edits

---
+++ src/generator.py
@@ -0,0 +1,16 @@
+class generators():
+    
+    @staticmethod
+    def generatehw(self):
+    """
+    Generate Hardware Description Yaml File 
+    Iterate over Generated Hardware Description
+
+    """
+   
+
+    @staticmethod
+    def idealcharacteristics(self):
+    """
+    Ideal Execution Scheduling and generating characteristics by solving bottlenecks
+    """
