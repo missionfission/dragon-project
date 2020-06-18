74031d109cec6ab66212a35acdf7a18bd729a8e0
Khushal Sethi <iaskhushal@gmail.com>
2020-06-18 05:45:39 +0530

    edits

--- src/generator.py
+++ src/generator.py
@@ -1,3 +1,4 @@
+import numpy as np
 import yaml
 import yamlordereddictloader
 
@@ -50,19 +51,20 @@ class Generator:
         """
         Execution statistics also have to be generated : Area, Energy, Time/Number of Cycles 
         """
+        mem_area = np.zeros((scheduler.mle))
+        compute_area = 0
+        total_area = np.sum(mem_area) + compute_area
+
+        mem_energy = np.zeros((scheduler.mle))
+        compute_energy = 0
+        total_energy = np.sum(mem_energy) + compute_energy
 
         scheduler.logger.info("Tool Output")
         scheduler.logger.info("===========================")
         scheduler.logger.info("Total No of cycles  = %d ", scheduler.total_cycles)
-        scheduler.logger.info(
-            "Memory Energy Consumption  = %d ", scheduler.total_cycles
-        )
-        scheduler.logger.info(
-            "Compute Energy Consumption  = %d ", scheduler.total_cycles
-        )
-        scheduler.logger.info(
-            " Total Energy Consumption  = %d ", scheduler.total_cycles
-        )
-        scheduler.logger.info("Memory Area Consumption  = %d ", scheduler.total_cycles)
-        scheduler.logger.info("Compute Area Consumption  = %d ", scheduler.total_cycles)
-        scheduler.logger.info("Total Area Consumption  = %d ", scheduler.total_cycles)
+        scheduler.logger.info("Memory Energy Consumption  = %d ", np.sum(mem_energy))
+        scheduler.logger.info("Compute Energy Consumption  = %d ", compute_energy)
+        scheduler.logger.info(" Total Energy Consumption  = %d ", total_energy)
+        scheduler.logger.info("Memory Area Consumption  = %d ", np.sum(mem_area))
+        scheduler.logger.info("Compute Area Consumption  = %d ", compute_area)
+        scheduler.logger.info("Total Area Consumption  = %d ", total_area)
--- src/utils/tables.py
+++ src/utils/tables.py
@@ -1,3 +1,10 @@
+import pandas as pd
+
+## Load Energy Tables 
+## Load Latency tables
+## Load Area Tables 
+
+
 class plugins():
     
     def __init__(self):
