9348047f171825fb0faa9d369ff3fac34317718e
Khushal Sethi <iaskhushal@gmail.com>
2020-06-18 16:53:33 +0530

    edits

--- src/generator.py
+++ src/generator.py
@@ -4,14 +4,11 @@ import yamlordereddictloader
 
 
 class Generator:
-    def __init__(self, constraintfiles=["max.yaml", "min.yaml"]):
+    def __init__(self, constraintfiles="max.yaml"):
         base_dir = "configs/"
 
         self.maxval = yaml.load(
             open(base_dir + constraintfiles[0]), Loader=yamlordereddictloader.Loader
-        )
-        self.minval = yaml.load(
-            open(base_dir + constraintfiles[1]), Loader=yamlordereddictloader.Loader
         )
 
     def writehwfile(self, content, filename):
@@ -38,13 +35,57 @@ class Generator:
         Energy high due to high of everything but compute is slow
         Where is area getting consumed the most?
         """
-        compute_idle_time()
-        increase_memory_size
-        increase_memory_bandwidth
-        increase_compute_arrays
+        # compute_idle_time
+        # increase_memory_size
+        # increase_memory_bandwidth/memory_connections
+        # increase number of compute_arrays
+        # change size of compute_arrays
+        # change number of memory banks
+        # Are Values Realistic : is the value of bandwidth and number of connections etc < maxval
+        # Is the size of memory array < maxval
         # Check if noc time is ever going to be a problem ?
         # Check whether minval and maxval satisfy here ?
-        arevaluesrealistic
+        newhw = scheduler.config
+        mm_compute = config["mm_compute"]
+        vector_compute = config["vector_compute"]
+
+        if config["mm_compute"]["class"] == "systolic_array":
+            config["mm_compute_per_cycle"] = (
+                ((mm_compute["size"]) ** 2) * mm_compute["N_PE"] / 2
+            )
+            config["comp_bw"] = (
+                mm_compute["size"] * mm_compute["N_PE"] * mm_compute["frequency"] * 2
+            )
+
+            self.logger.info(
+                "MM Compute per cycle : %d", config["mm_compute_per_cycle"]
+            )
+            self.logger.info("Compute Bandwidth Required : %d", config["comp_bw"])
+
+        if config["mm_compute"]["class"] == "mac":
+            config["mm_compute_per_cycle"] = (
+                ((mm_compute["size"]) ** 2) * mm_compute["N_PE"] / 2
+            )
+            config["comp_read_bw"] = (
+                mm_compute["size"] * mm_compute["N_PE"] * mm_compute["frequency"] * 2
+            )
+
+        for i in range(self.mle):
+            memory = config["memory"]["level" + str(i)]
+            self.mem_read_bw[i] = (
+                memory["frequency"]
+                * memory["banks"]
+                * memory["read_ports"]
+                * memory["width"]
+            )
+            self.mem_write_bw[i] = (
+                memory["frequency"]
+                * memory["banks"]
+                * memory["write_ports"]
+                * memory["width"]
+            )
+            self.mem_size[i] = memory["size"]
+
         return newhw
 
     def save_statistics(self, scheduler):
