e7c9af001ad6394bde302317b045b16d61085ccd
Khushal Sethi <iaskhushal@gmail.com>
2020-07-03 17:14:38 +0530

    edits

--- src/ir/node.py
+++ src/ir/node.py
@@ -54,15 +54,16 @@ class Node:
         return self.compute_expense, self.read_access, self.write_access
 
     def __repr__(self):
-        text = ", ".join([str(v) for v in self.outputs])
-        text += " = " + self.operator
-        if self.attributes:
-            text += (
-                "["
-                + ", ".join(
-                    [str(k) + " = " + str(v) for k, v in self.attributes.items()]
-                )
-                + "]"
-            )
-        text += "(" + ", ".join([str(v) for v in self.inputs]) + ")"
-        return text
+
+        # text = ", ".join([str(v) for v in self.outputs])
+        # text += " = " + self.operator
+        # if self.attributes:
+        #     text += (
+        #         "["
+        #         + ", ".join(
+        #             [str(k) + " = " + str(v) for k, v in self.attributes.items()]
+        #         )
+        #         + "]"
+        #     )
+        # text += "(" + ", ".join([str(v) for v in self.inputs]) + ")"
+        return self.operator
--- src/utils/visualizer.py
+++ src/utils/visualizer.py
@@ -1,4 +1,10 @@
 # Generate a Time Lapse Video
+from collections import namedtuple
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.ticker import MaxNLocator
 
 
 def roofline_model():
@@ -13,3 +19,23 @@ def generate_execution_movie():
 def genvideologger():
 
     pass
+
+
+def bar_graph(filename, var, xticklabels=None, xlabel=None, ylabel=None, bar_width=0.2):
+    fig, ax = plt.subplots(figsize=(30, 10))
+    base_dir = "figures/"
+    error_config = {"ecolor": "0.3"}
+    index = np.arange((len(var)))
+    ax.bar(
+        index, var, bar_width, color="blue", error_kw=error_config, label="",
+    )
+    ax.set_xticks(index)
+    ax.set_xticklabels(xticklabels)
+    plt.xticks(rotation=80)
+    plt.rc("xtick", labelsize=20)  # fontsize of the tick labels
+    plt.rc("ytick", labelsize=20)
+    ax.set_ylabel("Read Bandwidth", fontsize=20)
+    ax.set_xlabel("Graph Nodes", fontsize=20)
+    fig.tight_layout()
+    plt.savefig(base_dir + filename, bbox_inches="tight")
+    plt.show()
