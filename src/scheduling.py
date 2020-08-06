52815a1e7ea8c5e26587c58e5f8af8432cc3495e
Khushal Sethi <iaskhushal@gmail.com>
2020-08-07 05:15:45 +0530

    edits

--- src/scheduling.py
+++ src/scheduling.py
@@ -209,18 +209,3 @@ class Scheduling:
             )
 
         return config
-
-
-# if (read_bw_ll < self.mem_read_bw[self.mle - 1] and write_bw_ll < self.mem_write_bw[self.mle - 1]):
-# elif (
-#     read_bw_ll < self.mem_read_bw[self.mle - 1]
-#     and write_bw_ll > self.mem_write_bw[self.mle - 1]
-# ):
-#     step_cycles = write_bw_ll / self.mem_write_bw[self.mle - 1]
-# elif (
-#     read_bw_ll > self.mem_read_bw[self.mle - 1]
-#     and write_bw_ll < self.mem_write_bw[self.mle - 1]
-# ):
-#     step_cycles = read_bw_ll / self.mem_read_bw[self.mle - 1]
-# else:
-#     step_cycles = max(write_bw_ll / self.mem_write_bw[self.mle - 1])
