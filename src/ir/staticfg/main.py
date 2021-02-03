1828b2c4bfdd22941ac2c30d97eee7a1738a7373
Khushal <iaskhushal@gmail.com>
2021-02-03 13:53:52 +0530

    ast_update

--- src/ir/staticfg/main.py
+++ src/ir/staticfg/main.py
@@ -17,9 +17,9 @@ print(cfg)
 
 for node in cfg:
     print(node)
-    get_source(node)
-    for i in node.statements:
-            print(ast.dump(i))
+    node.get_source()
+    # for i in node.statements:
+    #         print(ast.dump(i))
 
 node_types = [ast.If, ast.For, ast.While, ast.FunctionDef, ast.Compare,ast.BinOp, ast.UnaryOp, ast.BoolOp]
-# ast.Name, ast.Attribute, ast.Str, ast.Subscript
+# ast.Name, ast.Attribute, ast.Str, ast.Subscript, ast.Assign, ast.Expr, ast.AugAssign
