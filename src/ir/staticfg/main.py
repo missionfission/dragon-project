f77c7ac0eac54df80085cea8343685378045c8f8
Khushal Sethi <iaskhushal@gmail.com>
2021-02-03 12:45:03 +0530

    edits

--- src/ir/staticfg/main.py
+++ src/ir/staticfg/main.py
@@ -1,13 +1,20 @@
-from staticfg import CFGBuilder
-
 import ast
 
-cfg = CFGBuilder().build_from_file('paillier.py','/home/khushal/code-anyhwperf/src/plugins/python-paillier/phe/paillier.py')
-cfg.build_visual('exampleCFG', 'pdf', show=False)
+from staticfg import CFGBuilder
+
+cfg = CFGBuilder().build_from_file(
+    "aes.py",
+    "/media/khushal/679f068d-921b-4d14-890f-3081c1728f98/research/SM/dragon-project/src/nonai_models/aes.py",
+)
+cfg.build_visual("exampleCFG", "pdf", show=False)
 print(cfg)
-for node in cfg:
-    for i in node.statements:
-        print(ast.dump(i))
+# for node in cfg:
+#     for i in node.statements:
+#         print(ast.dump(i))
 
 # for node in cfg.statements:
-    # print(node)
+# print(node)
+
+
+for node in cfg:
+    print(node)
