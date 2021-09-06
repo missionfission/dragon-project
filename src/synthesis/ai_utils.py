1513d846baf606ceaa164964241f0f84ff21682d
missionfission <iaskhushal@gmail.com>
2021-09-06 14:48:14 +0530

    edits

--- src/synthesis/ai_utils.py
+++ src/synthesis/ai_utils.py
@@ -77,40 +77,114 @@ def get_efficiency(graph_node, array_size):
 
     return efficiency
 
-def generate_loop_blocking_fast():
-    for ti, to, tb, orders in itertools.product(
-        Util.factorize(nested_loop_desc.loopcnt_ifm(), 3),
-        Util.factorize(nested_loop_desc.loopcnt_ofm(), 3),
-        Util.factorize(nested_loop_desc.loopcnt_bat(), 3),
-        itertools.product(
-            [None],
-            itertools.permutations((de.IFM, de.OFM)),
-            [None],
-            itertools.permutations((de.IFM, de.OFM)),
-        ),
-    ):
+    
+def _combine_search_lpbl_part2d(
+    resource,
+    cost,
+    nested_loop_desc,
+    layer_part,
+    batch_size,
+    part_lcurr,
+    part_lprev,
+    unit_nhops,
+    options,
+    layer_lb=None,
+):
+    """
+    Combine search the best loop blocking schemes with a certain parallel
+    partition scheme with the smallest combined cost.
 
-        if layer_lb is not None:
-            if layer_lb["ti"] is not None and ti != layer_lb["ti"]:
-                continue
-            if layer_lb["to"] is not None and to != layer_lb["to"]:
-                continue
-            if layer_lb["tb"] is not None and tb != layer_lb["tb"]:
-                continue
-            if layer_lb["orders"] is not None and orders != layer_lb["orders"]:
+    `nested_loop_desc` and `layer_part` are for the layer after partitioning.
+
+    `unit_nhops` is the unit number of hops for each data category for a single
+    access run.
+    """
+    layer_data_size = [0] * de.NUM
+    layer_data_size[de.FIL] = layer_part.total_filter_size()
+    layer_data_size[de.IFM] = layer_part.total_ifmap_size() * batch_size
+    layer_data_size[de.OFM] = layer_part.total_ofmap_size() * batch_size
+
+    def sweep():  # pylint: disable=missing-docstring
+
+        for cost_loop, dict_loop in _get_loopblocking_genfunc(options, layer_lb)(
+            resource, cost, nested_loop_desc, options, layer_lb
+        ):
+
+            if math.isinf(cost_loop):
                 continue
 
-        yield cost_loopblocking_gbuf_regf(
-            ti,
-            to,
-            tb,
-            orders,
-            resource=resource,
-            cost=cost,
-            nested_loop_desc=nested_loop_desc,
-            options=options,
-        )
+            # Calculate total number of hops, which determined by both
+            # partition scheme (number of hops per access) and loop blocking
+            # (runs of access).
+            access_mem = dict_loop["access"][me.DRAM]
+            assert len(access_mem) == de.NUM
+            access_runs = [a / float(s) for a, s in zip(access_mem, layer_data_size)]
+            total_nhops = [nh * r for nh, r in zip(unit_nhops, access_runs)]
 
+            # Partition cost.
+            cost_part = cost.nochop() * sum(total_nhops)
+            dict_part = {
+                "unit_nhops": unit_nhops,
+                "total_nhops": total_nhops,
+                "part_lprev": part_lprev.as_pod_type(),
+                "part_lcurr": part_lcurr.as_pod_type(),
+            }
+
+            # Combine.
+            dict_loop.update({"cost": cost_loop})
+            dict_part.update({"cost": cost_part})
+            yield (
+                cost_loop * resource.dim_nodes.size() + cost_part,
+                dict_loop,
+                dict_part,
+            )
+
+    return heapq.nsmallest(options.ntops, sweep(), key=lambda x: x[0])
+    end = time.time()
+    print("Execution Time", end-start)
+
+def adjust_reuse(reuse_, bl_cur, order_cur, bls_outer, orders_outer):
+        """
+        Adjust the data reuse based on special loop structures.
+
+        reuse_ is the reuse numbers for a specific level, e.g., reuse[BL.REGF].
+
+        This function is recursive as we need to look at the outer levels.
+        """
+        if ti[bl_cur] != 1 and to[bl_cur] != 1:
+            if order_cur.index(de.IFM) < order_cur.index(de.OFM):
+                # Loop ifm inside loop ofm.
+                # ofm also reused across current-level ifms.
+                reuse_[de.OFM] *= ti[bl_cur]
+            else:
+                # Loop ifm outside loop ofm.
+                # ifm also reused across current-level ofms.
+                reuse_[de.IFM] *= to[bl_cur]
+        elif ti[bl_cur] == 1 and to[bl_cur] != 1:
+            # Current level does not change ifm, so ifm reuses ofms.
+            reuse_[de.IFM] *= to[bl_cur]
+        elif ti[bl_cur] != 1 and to[bl_cur] == 1:
+            # Current level does not change ofm, so ofm reuses ifms.
+            reuse_[de.OFM] *= ti[bl_cur]
+        else:
+            assert ti[bl_cur] == 1 and to[bl_cur] == 1
+            # Current level loop counts are both 1 for ifms and ofms.
+            # Effectively this level does not change the buffered data in the
+            # inner level.
+            # See the outer level.
+            assert len(bls_outer) == len(orders_outer)
+            if len(bls_outer) > 0:
+                adjust_reuse(
+                    reuse_,
+                    bls_outer[0],
+                    orders_outer[0],
+                    bls_outer[1:],
+                    orders_outer[1:],
+                )
+
+
+def cost_loopblocking_gbuf_regf(tifm, tofm, tbat, orders, resource, cost, nested_loop_desc, options
+):
     ti = np.array(tifm)
     to = np.array(tofm)
     tb = np.array(tbat)
@@ -181,45 +255,6 @@ def generate_loop_blocking_fast():
     ## Adjusted size and reuse based on loop orders, bypass, etc..
 
     size = [None] * BL.NUM
-
-    def adjust_reuse(reuse_, bl_cur, order_cur, bls_outer, orders_outer):
-        """
-        Adjust the data reuse based on special loop structures.
-
-        reuse_ is the reuse numbers for a specific level, e.g., reuse[BL.REGF].
-
-        This function is recursive as we need to look at the outer levels.
-        """
-        if ti[bl_cur] != 1 and to[bl_cur] != 1:
-            if order_cur.index(de.IFM) < order_cur.index(de.OFM):
-                # Loop ifm inside loop ofm.
-                # ofm also reused across current-level ifms.
-                reuse_[de.OFM] *= ti[bl_cur]
-            else:
-                # Loop ifm outside loop ofm.
-                # ifm also reused across current-level ofms.
-                reuse_[de.IFM] *= to[bl_cur]
-        elif ti[bl_cur] == 1 and to[bl_cur] != 1:
-            # Current level does not change ifm, so ifm reuses ofms.
-            reuse_[de.IFM] *= to[bl_cur]
-        elif ti[bl_cur] != 1 and to[bl_cur] == 1:
-            # Current level does not change ofm, so ofm reuses ifms.
-            reuse_[de.OFM] *= ti[bl_cur]
-        else:
-            assert ti[bl_cur] == 1 and to[bl_cur] == 1
-            # Current level loop counts are both 1 for ifms and ofms.
-            # Effectively this level does not change the buffered data in the
-            # inner level.
-            # See the outer level.
-            assert len(bls_outer) == len(orders_outer)
-            if len(bls_outer) > 0:
-                adjust_reuse(
-                    reuse_,
-                    bls_outer[0],
-                    orders_outer[0],
-                    bls_outer[1:],
-                    orders_outer[1:],
-                )
 
     # regf.
     adjust_reuse(reuse[BL.REGF], BL.REGF, orders[me.REGF], [BL.GBUF], [orders[me.GBUF]])
@@ -296,50 +331,6 @@ def generate_loop_blocking_fast():
 
     return (cost_loop, dict_loop)
 
-    # comb_lpl_part2d
-    layer_data_size = [0] * de.NUM
-    layer_data_size[de.FIL] = layer_part.total_filter_size()
-    layer_data_size[de.IFM] = layer_part.total_ifmap_size() * batch_size
-    layer_data_size[de.OFM] = layer_part.total_ofmap_size() * batch_size
-
-    def sweep():  # pylint: disable=missing-docstring
-
-        for cost_loop, dict_loop in _get_loopblocking_genfunc(options, layer_lb)(
-            resource, cost, nested_loop_desc, options, layer_lb
-        ):
-
-            if math.isinf(cost_loop):
-                continue
-
-            # Calculate total number of hops, which determined by both
-            # partition scheme (number of hops per access) and loop blocking
-            # (runs of access).
-            access_mem = dict_loop["access"][me.DRAM]
-            assert len(access_mem) == de.NUM
-            access_runs = [a / float(s) for a, s in zip(access_mem, layer_data_size)]
-            total_nhops = [nh * r for nh, r in zip(unit_nhops, access_runs)]
-
-            # Partition cost.
-            cost_part = cost.nochop() * sum(total_nhops)
-            dict_part = {
-                "unit_nhops": unit_nhops,
-                "total_nhops": total_nhops,
-                "part_lprev": part_lprev.as_pod_type(),
-                "part_lcurr": part_lcurr.as_pod_type(),
-            }
-
-            # Combine.
-            dict_loop.update({"cost": cost_loop})
-            dict_part.update({"cost": cost_part})
-            yield (
-                cost_loop * resource.dim_nodes.size() + cost_part,
-                dict_loop,
-                dict_part,
-            )
-
-    return heapq.nsmallest(options.ntops, sweep(), key=lambda x: x[0])
-
-
 def factorize(value, num, limits=None):
     '''
     Factorize given `value` into `num` numbers. Return as a copy of num-length
@@ -399,6 +390,42 @@ def closest_factor(value, factor):
     res += (min(value, f), )
 
     return res
+
+
+import time
+def generate_loop_blocking_fast():
+    start = time.time()
+    for ti, to, tb, orders in itertools.product(
+        Util.factorize(nested_loop_desc.loopcnt_ifm(), 3),
+        Util.factorize(nested_loop_desc.loopcnt_ofm(), 3),
+        Util.factorize(nested_loop_desc.loopcnt_bat(), 3),
+        itertools.product(
+            [None],
+            itertools.permutations((de.IFM, de.OFM)),
+            [None],
+            itertools.permutations((de.IFM, de.OFM)),
+        ),
+    ):
+
+        if layer_lb is not None:
+            if layer_lb["ti"] is not None and ti != layer_lb["ti"]:
+                continue
+            if layer_lb["to"] is not None and to != layer_lb["to"]:
+                continue
+            if layer_lb["tb"] is not None and tb != layer_lb["tb"]:
+                continue
+            if layer_lb["orders"] is not None and orders != layer_lb["orders"]:
+                continue
+
+        yield cost_loopblocking_gbuf_regf(
+            to,
+            tb,
+            orders,
+            resource=resource,
+            cost=cost,
+            nested_loop_desc=nested_loop_desc,
+            options=options,
+        )
 
 # class ai_graph_manipulations():
 #   def __init__(graph):
