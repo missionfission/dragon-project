43de2ea942fda88eb110d271b8f7f090588f0514
Khushal <iaskhushal@gmail.com>
2021-10-06 19:22:00 -0700

    edits

--- src/nonai_models/kotana_reader.py
+++ src/nonai_models/kotana_reader.py
@@ -1,5 +1,6 @@
 import os
 import graphviz as gv
+import numpy as np
 def kotana_reader():
     file = open("../../vis-oos-16simd-32op-192-192reg.c0.txt",'r')
     lines = []
@@ -42,18 +43,64 @@ class Processor_Graph(object):
                            graph_attr={'label': self.name})
         id = 0
         i = 0
+        global pattern1, pattern2, pattern3, pattern4
+        pattern1_count = 0
+        pattern2_count = 0
+        pattern3_counts = np.zeros(len(pattern3))
+        pattern4_count = 0
+        pattern1_str = "Floating Matrix Add"
+        pattern2_str = "Integer ALU and Integer Shift"
+        pattern3_str = "Vector Matrix Multiplication"
+        pattern4_str = "Integer Addition"
         while i < len(self.nodes):
             node = self.nodes[i]
+      
+            if node == pattern1[0]:
+                if self.nodes[i+1] == pattern1[1]:
+                    i+= len(pattern1)-1
+                    pattern1_count+=1
+            if node == pattern2[0]:   
+                if self.nodes[i+1] == pattern2[1]:
+                   i+= len(pattern2)-1 
+                   pattern2_count+=1
+            if node == pattern4[0]:
+                if self.nodes[i+1] == pattern4[1]:
+                    i+= len(pattern4)-1
+                    pattern4_count+=1
             if node in pattern3:
                 count = 0
                 while self.nodes[i+1]==node:
                     count += 1
                     i+=1
-                graph.node(str(id),label=node+":"+str(count))
-                id +=1
-                if id>=2:
-                    graph.edge(str(id-2), str(id-1), label="next",_attributes={'style': 'dashed'})
+                pattern3_counts[pattern3.index(node)] = pattern3_counts[pattern3.index(node)] +count
+                # id +=1
+                # if id>=2:
+                    # graph.edge(str(id-2), str(id-1), label="next",_attributes={'style': 'dashed'})
             i+=1
+        
+        graph.node(str(0), label=pattern1_str+" x "+str(pattern1_count))
+        graph.node("0_expanded", label=",".join(pattern1))
+        
+        graph.node(str(1), label=pattern2_str+" x "+str(pattern2_count))
+        graph.node("1_expanded", label=",".join(pattern2))
+        
+        graph.node(str(2), label=pattern3_str)
+        pattern3_full = ""
+        for i, node in enumerate(pattern3):
+            pattern3_full += node + " x " + str(pattern3_counts[i]) + "\n"
+        graph.node("2_expanded", label=pattern3_full)
+        
+        graph.node(str(3), label=pattern4_str+" x "+str(pattern4_count))
+        graph.node("3_expanded", label=",".join(pattern4))
+
+        graph.edge(str(0), str(1), label="next",_attributes={'style': 'dashed'})
+        graph.edge(str(1), str(2), label="next",_attributes={'style': 'dashed'})
+        graph.edge(str(2), str(3), label="next",_attributes={'style': 'dashed'})
+        
+        graph.edge(str(0), "0_expanded", label="expansions",_attributes={'style': 'dashed'})
+        graph.edge(str(1), "1_expanded", label="expansions",_attributes={'style': 'dashed'})
+        graph.edge(str(2), "2_expanded", label="expansions",_attributes={'style': 'dashed'})
+        graph.edge(str(3), "3_expanded", label="expansions",_attributes={'style': 'dashed'})
         return graph
 
     def build_visual(self, filepath, format, calls=True, show=True):
