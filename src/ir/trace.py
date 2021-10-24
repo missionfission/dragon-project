[WIP] Fix bug and add message calculation for resnet & bert - Fix partial illusion_mapping indention bugs - Add resnet message calculation with real model or hacks in illusion_mapping = Add Bert message calculation but mapping not works

src/ir/trace.py
@@ -67,22 +67,21 @@ def trace(model, args=(), kwargs=None):
                     node.in_edge_mem = np.prod(node.inputs[0].shape)
                     node.out_edge_mem = np.prod(node.outputs[0].shape)
                     node.mem_util = node.weights + node.out_edge_mem
-                    print(
-                    "inputs",
-                    np.prod(node.inputs[0].shape),
-                    "outputs",
-                    np.prod(node.outputs[0].shape),
-                    "weights",
-                    node.weights,
-                    "total",
-                     np.prod(node.inputs[0].shape)+np.prod(node.outputs[0].shape)+node.weights
-                    )
+                    # print(
+                    # "inputs",
+                    # np.prod(node.inputs[0].shape),
+                    # "outputs",
+                    # np.prod(node.outputs[0].shape),
+                    # "weights",
+                    # node.weights,
+                    # "total",
+                    #  np.prod(node.inputs[0].shape)+np.prod(node.outputs[0].shape)+node.weights
+                    # )
                     # if not isinstance(node.mem_util, np.int64):
                     #     if (node.mem_util).shape[0] > 1:
                     #         node.mem_util = node.mem_util[0]
                     if node.compute_expense > 0:
                         nodes.append(node)
-
     for i, node in enumerate(nodes):
         if i < len(nodes) - 1:
             node.next = nodes[i + 1]

src/sweep/gen_testbench.py
@@ -4,6 +4,7 @@ from transformers import BertModel, BertConfig
 import torch
 import sys
 import numpy as np
+
 sys.path.append("/pool0/wynwyn/dragon-project/src")
 from ir.trace import trace
 
@@ -14,7 +15,6 @@ def gen_resnet(layers, block, **kwargs):
     block: BasicBlock or Bottleneck
     groups: int 
     width_per_group: int, 64*k
-    input_size: list, len(input_size) = 4
     
     For BasicBlock, resnet{sum(layer)*2+2}
     For Bottleneck, resnet{sum(layer)*3+2}_{group}x{width_per_group}d
@@ -22,7 +22,6 @@ def gen_resnet(layers, block, **kwargs):
     if block == "BasicBlock":
         model = ResNet(BasicBlock, layers)
         n_layer = sum(layers)*2 + 2
-        print(n_layer, layers)
     elif block == "Bottleneck":
         model = ResNet(Bottleneck, layers, groups=kwargs["groups"], width_per_group=kwargs["width_per_group"])
         n_layer = sum(layers)*3 + 2
@@ -31,34 +30,13 @@ def gen_resnet(layers, block, **kwargs):
     n_param = 0
     for param in model.parameters():
         n_param += np.prod(param.size())
-    inputs = torch.randn(kwargs["input_size"])
+    inputs = torch.randn([1, 3, 224, 224])
     resnet_graph = trace(model.eval(), inputs)
     # return resnet_graph, n_param
-    return n_param, n_layer
+    return n_param, n_layer, resnet_graph
 
-
-def sweep():
-    groups = [1, 16] #[1, 2, 4, 8, 16]
-    width_per_group = [64, 64*(2**3)] #[64*(2**i) for i in range(11)]
-    input_size = [1, 3, 224, 224]
-    layer_sizes = [[1, 1, 1, 1], [1024, 1024, 1024, 1024]]#[(2**i, 2**j, 2**k, 2**l) for i in range(11) for j in range(11) for k in range(11) for l in range(11)]
-
-    model_list = []
-
-    for l1, l2, l3, l4 in layer_sizes:
-        print(l1, l2, l3, l4)
-        # BasicBlock
-        n_param, n_layer = gen_resnet([l1, l2, l3, l4], "BasicBlock", input_size=input_size)
-        model_list.append([n_param, n_layer, "BasicBlock_{}_{}_{}_{}".format(l1, l2, l3, l4)])
-        # Bottleneck
-        for group in groups:
-            for w in width_per_group:
-                n_param, n_layer = gen_resnet([l1, l2, l3, l4], "Bottleneck", input_size=input_size, groups=group, width_per_group=w)
-                model_list.append([n_param, n_layer, "Bottleneck_{}_{}_{}_{}_{}g_{}b".format(l1, l2, l3, l4, group, w)])
-    return model_list
-
-def gen_bert():
-    configuration = BertConfig(num_hidden_layers=12)
+def gen_bert(layers):
+    configuration = BertConfig(num_hidden_layers=layers)
     model = BertModel(configuration)
 
     indexed_tokens = [
@@ -81,29 +59,11 @@ def gen_bert():
     ]
 
     tokens_tensor = torch.tensor([indexed_tokens])
-    print(model.eval())
-    print(len(list(model.parameters())))
-
-
     model(tokens_tensor)
 
-    bert_graph = trace(model, tokens_tensor)
-    print(bert_graph)
+    bert_graph = trace(model.eval(), tokens_tensor)
     n_param = 0
     for param in model.parameters():
         n_param += np.prod(param.size())
-    return n_param
+    return n_param, len(list(model.parameters())), bert_graph
 
-if __name__ == "__main__":
-    # 
-    # print("{:e}".format(gen_bert()))
-    # exit()
-    model_list = [gen_resnet([3, 8, 36, 3], "Bottleneck", input_size=[1, 3, 224, 224], groups=1, width_per_group=64)]
-    model_list = sorted(model_list, key=lambda x: x[0])
-    print("#params min {:e}, max {:e}".format(model_list[0][0], model_list[-1][0]))
-    sorted(model_list, key=lambda x: x[1])
-    print("#layers min {}, max {}".format(model_list[0][1], model_list[-1][1]))
-
-
-    # Chip size = n_param / n_chip * constant_scale
-    # illusion_mapping(graph, n_chips, 1, n_param/n_chips, deeper=True, wider=False)