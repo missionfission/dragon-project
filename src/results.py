c53d56ad020d2caa5d9f86821a155bdb2e259f8b
Khushal Sethi <29970524+missionfission@users.noreply.github.com>
2021-12-05 09:31:38 -0800

    Update results.py

--- src/results.py
+++ src/results.py
@@ -5,8 +5,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--node",default=3)
 parser.add_argument("--workload",default='resnet18')
 parser.add_argument("--pitch", default="1um")
-parser.add_argument("--",default="")
-parser.add_argument("--",default="")
+parser.add_argument("--backprop",default=False)
+parser.add_argument("--mapping",default="edp")
 
 args = parser.parse_args()
 
