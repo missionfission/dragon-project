f55a03dac483ad5dede74ea97cc586dd01ce9faf
Khushal Sethi <29970524+missionfission@users.noreply.github.com>
2021-12-05 09:43:53 -0800

    Update results.py

--- src/results.py
+++ src/results.py
@@ -7,6 +7,7 @@ parser.add_argument("--workload",default='resnet18')
 parser.add_argument("--pitch", default="1um")
 parser.add_argument("--backprop",default=False)
 parser.add_argument("--mapping",default="edp")
+parser.add_argument("",default="")
 
 args = parser.parse_args()
 
